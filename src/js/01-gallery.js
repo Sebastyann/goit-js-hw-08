2b104051af9140365f8864000e992af1d11b4924
Sebastyann <sebik55555@gmail.com>
2022-12-14 09:52:40 +0200

    Update 01-gallery.js

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,6 +1,4 @@
 // Add imports above this line
 import { galleryItems } from './gallery-items';
 // Change code below this line
-heloo = 'hello world';
-console.log(heloo);
 console.log(galleryItems);
