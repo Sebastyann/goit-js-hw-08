63f4af3c0173532834162f8144d2e68966efae52
Sebastyann <sebik55555@gmail.com>
2022-12-19 16:15:02 +0200

    add

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,5 +1,4 @@
 import throttle from 'lodash.throttle';
-
 const form = document.querySelector('.feedback-form');
 const email = document.querySelector('input[name="email"]');
 const message = document.querySelector('textarea[name="message"]');
@@ -12,14 +11,12 @@ form.addEventListener(
     localStorage.setItem(LOCALSTORAGE_KEY, JSON.stringify(objectToSave));
   }, 500)
 );
-
 form.addEventListener('submit', e => {
   e.preventDefault();
   console.log({ email: email.value, message: message.value });
   form.reset();
   localStorage.removeItem(LOCALSTORAGE_KEY);
 });
-
 const load = key => {
   try {
     const serializedState = localStorage.getItem(key);
@@ -28,7 +25,6 @@ const load = key => {
     console.error('Get state error: ', error.message);
   }
 };
-
 const storageData = load(LOCALSTORAGE_KEY);
 if (storageData) {
   email.value = storageData.email;
